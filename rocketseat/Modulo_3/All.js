6ca6a7932128e771e8a3b278fba302b944205c64
Luan Alves <luan.alves@comunike.me>
2019-06-25 09:03:18 -0300

    feat: modulo 3

---
+++ rocketseat/Modulo_3/All.js
@@ -0,0 +1,48 @@
+var elementlist = document.querySelector("#app ul");
+var elementinput = document.querySelector("#app input");
+var elementbutton = document.querySelector("#app button");
+
+var all = ["fazer coffe", "learning", "Community"];
+
+function lerTodos() {
+  elementlist.innerHTML = "";
+
+  for (todo of all) {
+    var ElementAll = document.createElement("li");
+    var ElementText = document.createTextNode(todo);
+
+    var linkElement = document.createElement("a");
+
+    linkElement.setAttribute("href", "#");
+
+    var pos = all.indexOf(todo);
+    linkElement.setAttribute("onclick", "deletet(" + pos + ")");
+    var linkText = document.createTextNode(" Excluir");
+
+    linkElement.appendChild(linkText);
+
+    ElementAll.appendChild(ElementText);
+    ElementAll.appendChild(linkElement);
+    elementlist.appendChild(ElementAll);
+  }
+}
+
+lerTodos();
+
+function add() {
+  textelement = elementinput.value;
+  if (textelement == "") {
+    alert("Campo vazio");
+  } else {
+    all.push(textelement);
+    elementinput.value = "";
+    lerTodos();
+  }
+}
+
+elementbutton.onclick = add;
+
+function deletet(pos) {
+  all.splice(pos, 1);
+  lerTodos();
+}
