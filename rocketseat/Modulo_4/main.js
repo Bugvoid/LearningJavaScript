a850d13a439ccd13389d849945af869c3ed14497
Luan Alves <luan.alves@comunike.me>
2019-06-27 10:27:01 -0300

    feat: use axios and exercicios

--- rocketseat/Modulo_4/main.js
+++ rocketseat/Modulo_4/main.js
@@ -1,10 +1,36 @@
-var xhr = new XMLHttpRequest();
+// var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "http://api.github.com/users/bugvoid");
-xhr.send(null);
+// xhr.open("GET", "http://api.github.com/users/bugvoid");
+// xhr.send(null);
 
-xhr.onreadystatechange = function() {
-  if (xhr.readyState === 4) {
-    console.log(JSON.parse(xhr.responseText));
-  }
-};
+// xhr.onreadystatechange = function() {
+//   if (xhr.readyState === 4) {
+//     console.log(JSON.parse(xhr.responseText));
+//   }
+// };
+
+// var minhapromise = function() {
+//   return new Promise(function(resolve, reject) {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open("GET", "http://api.github.com/users/bugvoid");
+//     xhr.send(null);
+
+//     xhr.onreadystatechange = function() {
+//       if (xhr.readyState === 4) {
+//         if (xhr.status === 200) {
+//           resolve(JSON.parse(xhr.responseText));
+//         } else {
+//           reject("ERro");
+//         }
+//       }
+//     };
+//   });
+// };
+
+// axios.get('https://api.github.com/users/bugvoid')
+//   .then(function(response) {
+//     console.log(response);
+//   })
+//   .catch(function(error) {
+//     console.log(error);
+//   });
